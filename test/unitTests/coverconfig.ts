fd3fc9cef9b865e0f8b054d06855210510fa8944
Piotr Puszkiewicz <piotrp@microsoft.com>
2017-11-08 15:41:50 -0800

    WiP

---
+++ test/unitTests/coverconfig.ts
@@ -0,0 +1,14 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+export default {
+    "enabled": true,
+    "relativeSourcePath": "../../src",
+    "relativeCoverageDir": "./coverage",
+    "ignorePatterns": ["**/node_modules/**"],
+    "includePid": false,
+    "reports": ["json", "html", "lcov"],
+    "verbose": false
+}
